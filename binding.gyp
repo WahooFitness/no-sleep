471a2a0bf7a16ad2b7c41722d669de406e91b90b
Ryan Lamb <ryan@thesufferfest.com>
2019-03-05 09:54:02 -0800

    Initial implementation.

---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+  "targets": [
+    {
+      "target_name": "nosleep",
+      "sources": [ "no-sleep.cc" ]
+    }
+  ]
+}
---
+++ no-sleep.cc
@@ -0,0 +1,33 @@
+#ifdef WIN32 
+#include <node.h>
+
+namespace nosleep {
+
+using node::AtExit;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::NewStringType;
+using v8::Object;
+using v8::String;
+using v8::Value;
+//BluetoothAPIs.h depends on these.
+#include <Winsock2.h>
+#include <Ws2bth.h>
+
+#include <BluetoothAPIs.h>
+#pragma comment(lib, "Bthprops.lib")
+
+void PreventSleep(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED) != NULL);
+}
+
+void Initialize(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "preventSleep", PreventSleep);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+
+}
+#endif
