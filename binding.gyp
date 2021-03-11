084487cceb55f86ca36247f12ac38560d64df210
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2021-03-11 15:12:42 -0800

    Add napi

--- binding.gyp
+++ binding.gyp
@@ -2,7 +2,12 @@
   "targets": [
     {
       "target_name": "nosleep",
-      "sources": [ "no-sleep.cc" ]
+      "sources": [ "no-sleep.cc" ],
+      "include_dirs": [
+        "<!@(node -p \"require('node-addon-api').include\")"
+      ],
+      "dependencies": ["<!(node -p \"require('node-addon-api').gyp\")"],
+      "defines": ["NAPI_DISABLE_CPP_EXCEPTIONS"]
     }
   ]
 }
--- no-sleep.cc
+++ no-sleep.cc
@@ -1,39 +1,21 @@
 #ifdef WIN32 
-#include <node.h>
+#include <napi.h>
 
-namespace nosleep {
-
-using node::AtExit;
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::NewStringType;
-using v8::Object;
-using v8::String;
-using v8::Value;
-//BluetoothAPIs.h depends on these.
-#include <Winsock2.h>
-#include <Ws2bth.h>
-
-#include <BluetoothAPIs.h>
-#pragma comment(lib, "Bthprops.lib")
-
-void PreventSleep(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED) != NULL);
+Napi::Boolean PreventSleep(const Napi::CallbackInfo &info) {
+  return Napi::Boolean::New(info.Env(), SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED) != NULL);
 }
 
-void AllowSleep(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(SetThreadExecutionState(ES_CONTINUOUS) != NULL);
+Napi::Boolean AllowSleep(const Napi::CallbackInfo &info) {
+  return Napi::Boolean::New(info.Env(), SetThreadExecutionState(ES_CONTINUOUS) != NULL);
 }
 
-void Initialize(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "preventSleep", PreventSleep);
-  NODE_SET_METHOD(exports, "allowSleep", AllowSleep);
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+  exports.Set(Napi::String::New(env, "preventSleep"),
+    Napi::Function::New(env, PreventSleep));
+  exports.Set(Napi::String::New(env, "allowSleep"),
+    Napi::Function::New(env, AllowSleep));
 }
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
-
-}
+NODE_API_MODULE(addon, Init)
 #endif
